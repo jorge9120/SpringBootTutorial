0d3899d5ee80a37bc926fb70fc532eb8c6cd9511
jorgeTech <jorge.tech20@mail.com>
2019-07-16 21:12:09 -0500

    cambios base de datos e interfaz

---
+++ sistemaIngreso/src/main/java/com/crud/ejemplo/entity/roles.java
@@ -0,0 +1,41 @@
+package com.crud.ejemplo.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+
+public class roles implements Serializable{
+	@Column
+ private Long id;
+	@Column
+ private String nombre;
+	@Column
+ private String descripcion;
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getDescripcion() {
+	return descripcion;
+}
+public void setDescripcion(String descripcion) {
+	this.descripcion = descripcion;
+}
+ 
+ 
+ 
+}
